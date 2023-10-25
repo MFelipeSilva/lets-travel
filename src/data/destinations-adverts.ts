42845f8f82f300cdd24148947570ed7f3a89cf93
Felipe da Silva <silvaafelipe016@gmail.com>
2023-10-25 14:16:19 -0300

    feat: removed some images and converted to links

--- src/data/destinations-adverts.ts
+++ src/data/destinations-adverts.ts
@@ -1,31 +1,25 @@
-import imagem_rj from "../../src/assets/imagem-rj.jpg";
-import imagem_sp from "../../src/assets/imagem-sp.jpg";
-import imagem_ma from "../../src/assets/imagem-ma.jpg";
-import imagem_gm from "../../src/assets/imagem-gm.jpg";
-import imagem_sa from "../../src/assets/imagem-sa.jpg";
-
 export const destinations_adverts = [
   {
     id: "5",
-    img: imagem_rj,
+    img: "https://uploaddeimagens.com.br/images/004/647/331/full/imagem-rj.jpg?1698253670",
     title: "Rio de Janeiro",
     description: "Confira as ofertas",
   },
   {
     id: "6",
-    img: imagem_sp,
+    img: "https://uploaddeimagens.com.br/images/004/647/335/full/imagem-sp.jpg?1698253715",
     title: "São Paulo",
     description: "Confira as ofertas",
   },
   {
     id: "7",
-    img: imagem_ma,
+    img: "https://uploaddeimagens.com.br/images/004/647/338/full/imagem-ma.jpg?1698253746",
     title: "Maceió",
     description: "Confira as ofertas",
   },
   {
     id: "8",
-    img: imagem_gm,
+    img: "https://uploaddeimagens.com.br/images/004/647/342/full/imagem-gm.jpg?1698253798",
     title: "Gramado",
     description: "Confira as ofertas",
   },
